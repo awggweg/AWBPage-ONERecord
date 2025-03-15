8debe14e5744a440f4d572ebd988ed4c5002e996
Lin <220340138@cauc.edu.cn>
2025-03-15 14:57:27 +0800

    Complete involvedParties

--- backEnd/sparql_queries.py
+++ backEnd/sparql_queries.py
@@ -1,17 +1,57 @@
 class InvolvedParty:
-    SHIPPERNAME="""PREFIX cargo: <https://onerecord.iata.org/ns/cargo#> 
-                    PREFIX code: <https://onerecord.iata.org/ns/code-lists/> 
-                    SELECT ?name 
+    shipper="""PREFIX cargo: <https://onerecord.iata.org/ns/cargo#>
+                PREFIX ParticipantIdentifier:  <https://onerecord.iata.org/ns/code-lists/ParticipantIdentifier#>
+                SELECT ?Name ?Address 
+                WHERE { 
+                        ?waybill a cargo:Waybill.
+                        ?waybill cargo:involvedParties [ 
+                        <https://onerecord.iata.org/ns/cargo#partyRole>  ParticipantIdentifier:SHP ;
+                        cargo:partyDetails [
+                            cargo:name ?Name ;
+                            cargo:basedAtLocation/cargo:locationName ?Address
+                        ]
+                    ]   
+                }"""
+    consinee="""PREFIX cargo: <https://onerecord.iata.org/ns/cargo#> 
+                    PREFIX ParticipantIdentifier: <https://onerecord.iata.org/ns/code-lists/ParticipantIdentifier#>
+                    SELECT ?Name ?Address
                     WHERE { 
-                        ?party a cargo:Party ; 
-                        cargo:partyRole <https://onerecord.iata.org/ns/code-lists/ParticipantIdentifier#SHP> ; 
-                        cargo:partyDetails [ cargo:name ?name ] . 
+                            ?waybill cargo:involvedParties [ 
+                            cargo:partyRole ParticipantIdentifier:CNE ;
+                            cargo:partyDetails [
+                                cargo:name ?Name ;
+                                cargo:basedAtLocation/cargo:locationName ?Address
+                            ]
+                            ]
                     }"""
-    CONSIGNEENAME="""PREFIX cargo: <https://onerecord.iata.org/ns/cargo#> 
-                    PREFIX code: <https://onerecord.iata.org/ns/code-lists/> 
-                    SELECT ?name 
+    airline="""PREFIX cargo: <https://onerecord.iata.org/ns/cargo#> 
+                    PREFIX ParticipantIdentifier: <https://onerecord.iata.org/ns/code-lists/ParticipantIdentifier#>
+                    SELECT ?Name ?Airlinecode
                     WHERE { 
-                        ?party a cargo:Party ; 
-                        cargo:partyRole <https://onerecord.iata.org/ns/code-lists/ParticipantIdentifier#CNE> ; 
-                        cargo:partyDetails [ cargo:name ?name ] .
+                            ?waybill cargo:involvedParties [ 
+                            cargo:partyRole ParticipantIdentifier:AIR ;
+                            cargo:partyDetails [
+                                cargo:name ?Name ;
+                                cargo:airlineCode ?Airlinecode
+                            ]
+                        ]
                     }"""
+    
+    carrierAgent="""PREFIX cargo: <https://onerecord.iata.org/ns/cargo#> 
+                    PREFIX ParticipantIdentifier: <https://onerecord.iata.org/ns/code-lists/ParticipantIdentifier#>
+                    SELECT ?Name
+                    WHERE { 
+                        ?waybill cargo:involvedParties [ 
+                        cargo:partyRole ParticipantIdentifier:AGT ;
+                        cargo:partyDetails [
+                            cargo:name ?Name
+                            ]
+                        ]
+                    }"""
+    accountingInformation="""PREFIX cargo: <https://onerecord.iata.org/ns/cargo#>
+                            SELECT ?accountingNoteText
+                            WHERE { 
+                                ?waybill cargo:accountingNotes [ 
+                                cargo:accountingNoteText ?accountingNoteText ;
+                                ]
+                            }"""
